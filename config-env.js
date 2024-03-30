8e5ea2d64cc46de541c17e4df2d7ea2a32936799
underrated527 <163488071+underrated527@users.noreply.github.com>
2024-03-30 05:46:43 +0300

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
-BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
-PASSWORD: process.env.PASSWORD || "Enter you like password",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD_e28xFMFp",
+BOT_NUMBER: process.env.BOT_NUMBER || "254 740 419256",
+PASSWORD: process.env.PASSWORD || "kanko3692",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
 };
